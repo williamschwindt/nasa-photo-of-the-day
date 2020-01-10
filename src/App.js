781f516c6b4d1313fcbdf1d375494e4daa9c057a
William Schwindt <william@Williams-MacBook-Pro.local>
2020-01-09 18:11:20 -0800

    advanced styling added, date button rerender in progress

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from 'axios';
 import Photo from './components/Photo';
+import styled from 'styled-components';
+
+const P = styled.p`
+  font-size: 1.2rem;
+  margin-top: 20px;
+`;
+
+const Input = styled.input`
+  text-align: center;
+  border-radius: 10px;
+  border: 1px solid black;
+`;
 
 function App() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+  var yyyy = today.getFullYear();
+
+  today = yyyy.toString() + '-' + mm.toString() + '-' + dd.toString();
+
   const [photo, setPhoto] = useState([]);
+
+  const [day, setDay] = useState(today);
+  console.log(day);
 
   useEffect(() => {
     axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=5OEsUVedFGlY1ldUSWVhkLq3hXv1rda1APz6bfRT")
+      .get(`https://api.nasa.gov/planetary/apod?api_key=5OEsUVedFGlY1ldUSWVhkLq3hXv1rda1APz6bfRT&date=${day}`)
       .then((res) => {
         console.log(res);
         setPhoto(res.data);
@@ -23,6 +45,8 @@ function App() {
 
   return (
     <div className="App">
+      <P>Choose the day!</P>
+      <Input placeholder={"yyyy-mm-dd"} onChange={(e) => setDay(e.target.value)}></Input>
       <Photo title={photo.title} scr={photo.url} description={photo.explanation} date={photo.date}/>
     </div>
   );
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+display: flex;
+flex-direction: column;
+width: 50%;
+justify-content: center;
+margin: 0 auto;
+`;
+
+const Img = styled.img`
+    width: 400px;
+    border: 1px solid white;
+    margin: 30px auto;
+`;
+
+const P = styled.p`
+    font-size: 1.2rem;
+    margin:  0 30px;
+`;
 
 function Photo(props) {
     return(
         <div>
             <h1>{props.title}</h1>
-            <p>{props.date}</p>
-            <div className="content">
-                <img src={props.scr} alt="img of the day" />
+            <Div>
+                <Img src={props.scr} alt="img of the day" />
                 <div>
-                    <p>{props.description}</p>
+                    <P>{props.description}</P>
                 </div>
-            </div>
+            </Div>
         </div>
     )
 }
